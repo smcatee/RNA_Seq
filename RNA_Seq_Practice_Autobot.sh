483fa2b33434fa8ddacac3670aa3f799636750ab
Sean David McAtee <sean.d.mcatee@gmail.com>
2020-02-06 21:17:53 -0800

    added autobot conversion from SRA to sam to bam

--- RNA_Seq_Practice_Autobot.sh
+++ RNA_Seq_Practice_Autobot.sh
@@ -4,16 +4,22 @@
 ##Version: 1/22/2020
 
 ##About: This script will run through each step of mRNA Differential Expression Analysis as outlined in the workflow document.
+# The following programs were set as PATH variables:
+    SRA Toolkit, FASTX-Toolkit,
+# The following programs were set as alias:
+#   samtools,
 
-currentDate=`date +"%Y_%m_%d"`
+currentDate=`date +"%F_%H%M"`
 mkdir -p RNA_Seq_Run_${currentDate}/{SRA_Files/{Raw_BAM_Files,Trimmed_BAM_Files,QC_Output},Aligner_Output/{Trinity,BowTie,STAR,Oasis,QC}}
+cd RNA_Seq_Run_${currentDate}
 
 # Download SRA files
-curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra75/SRR/009750/SRR9984971 | sam-dump | /Applications/samtools/view -bS
-curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra19/SRR/009750/SRR9984973
-curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra23/SRR/009750/SRR9984974
+curl -o SRA_Files/Raw_BAM_Files/SRR9984971 https://sra-download.ncbi.nlm.nih.gov/traces/sra75/SRR/009750/SRR9984971
+curl -o SRA_Files/Raw_BAM_Files/SRR9984973 https://sra-download.ncbi.nlm.nih.gov/traces/sra19/SRR/009750/SRR9984973
+curl -o SRA_Files/Raw_BAM_Files/SRR9984974 https://sra-download.ncbi.nlm.nih.gov/traces/sra23/SRR/009750/SRR9984974
 
-# Convert SRA to SAM and directly into BAM to save space
-for f in *; do sam-dump ${f} | /Applications/samtools/view -bS - > "${f}.bam"; done
 
-#
+# Convert SRA to SAM and directly into BAM for FastQC and other tools
+for f in SRA_Files/Raw_BAM_Files/*; do sam-dump ${f} | samtools view -bS - > "${f}.bam"; done
+
+# Check that BAM files were created and erase SRA
