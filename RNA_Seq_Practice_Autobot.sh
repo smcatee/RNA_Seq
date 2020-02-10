1a0ac70d725d288c7a55f96412a8ac86dcb548c3
smcatee <35880838+smcatee@users.noreply.github.com>
2020-02-10 10:50:31 -0800

    Update RNA_Seq_Practice_Autobot.sh

--- RNA_Seq_Practice_Autobot.sh
+++ RNA_Seq_Practice_Autobot.sh
@@ -14,15 +14,24 @@ mkdir -p RNA_Seq_Run_${currentDate}/{SRA_Files/{Raw_BAM_Files,Trimmed_BAM_Files,
 cd RNA_Seq_Run_${currentDate}
 
 # Download SRA files
-curl -o SRA_Files/Raw_BAM_Files/SRR9984971 https://sra-download.ncbi.nlm.nih.gov/traces/sra75/SRR/009750/SRR9984971
-curl -o SRA_Files/Raw_BAM_Files/SRR9984973 https://sra-download.ncbi.nlm.nih.gov/traces/sra19/SRR/009750/SRR9984973
-curl -o SRA_Files/Raw_BAM_Files/SRR9984974 https://sra-download.ncbi.nlm.nih.gov/traces/sra23/SRR/009750/SRR9984974
+#curl -o SRA_Files/Raw_BAM_Files/SRR9984971 https://sra-download.ncbi.nlm.nih.gov/traces/sra75/SRR/009750/SRR9984971
+#curl -o SRA_Files/Raw_BAM_Files/SRR9984973 https://sra-download.ncbi.nlm.nih.gov/traces/sra19/SRR/009750/SRR9984973
+#curl -o SRA_Files/Raw_BAM_Files/SRR9984974 https://sra-download.ncbi.nlm.nih.gov/traces/sra23/SRR/009750/SRR9984974
+
+# Download fastq files (paired end reads)
+curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984971/Mc_4313-R1.fastq.1
+curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984971/Mc_4313-R2.fastq.1
+
+curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984973/Mc_4271-R1.fastq.1
+curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984973/Mc_4271-R2.fastq.1
+
+curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984974/Mc_4297-R1.fastq.1
+curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984974/Mc_4297-R2.fastq.1
 
 
 # Convert SRA to SAM and directly into BAM for FastQC and other tools
-for f in SRA_Files/Raw_BAM_Files/*; do sam-dump ${f} | samtools view -bS - > "${f}.bam"; done
+#for f in SRA_Files/Raw_BAM_Files/*; do sam-dump ${f} | samtools view -bS - > "${f}.bam"; done
 
-# Check that BAM files were created and erase SRA  ?maybe?
 
 
 # Pass split PE reads to trimmomatic
