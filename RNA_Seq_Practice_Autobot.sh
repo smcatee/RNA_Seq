192345f43e5ef96512fbf8120c806927f1aeac9e
Sean David McAtee <sean.d.mcatee@gmail.com>
2020-02-16 14:03:06 -0600

    fastq-dump split and checksum verification

--- RNA_Seq_Practice_Autobot.sh
+++ RNA_Seq_Practice_Autobot.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 
 ##Author: Sean McAtee
-##Version: 1/22/2020
+##Version: 2/16/2020
 
 ##About: This script will run through each step of mRNA Differential Expression Analysis as outlined in the workflow document.
-# The following programs were set as PATH variables:
-    SRA Toolkit, FASTX-Toolkit,
-# The following programs were set as alias:
-#   samtools,
+
+# PATH variables:
+#   SRA Toolkit, FASTX-Toolkit
+# Alias variable names:
+#   alias samtools='/Applications/samtools/samtools'
+#   alias trimmomatic='java -jar /Applications/Trimmomatic-0.39/trimmomatic-0.39.jar'
+
+# Enter the accession number that you would like to run through Autobot
+#   Tested on: SRR9984971, SRR9984973, SRR9984974
+SRA_Accession_Number="SRR9984971"
 
 currentDate=`date +"%F_%H%M"`
-mkdir -p RNA_Seq_Run_${currentDate}/{SRA_Files/{Raw_BAM_Files,Trimmed_BAM_Files,QC_Output},Aligner_Output/{Trinity,BowTie,STAR,Oasis,QC}}
-cd RNA_Seq_Run_${currentDate}
-
-# Download SRA files
-#curl -o SRA_Files/Raw_BAM_Files/SRR9984971 https://sra-download.ncbi.nlm.nih.gov/traces/sra75/SRR/009750/SRR9984971
-#curl -o SRA_Files/Raw_BAM_Files/SRR9984973 https://sra-download.ncbi.nlm.nih.gov/traces/sra19/SRR/009750/SRR9984973
-#curl -o SRA_Files/Raw_BAM_Files/SRR9984974 https://sra-download.ncbi.nlm.nih.gov/traces/sra23/SRR/009750/SRR9984974
-
-# Download fastq files (paired end reads)
-curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984971/Mc_4313-R1.fastq.1
-curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984971/Mc_4313-R2.fastq.1
-
-curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984973/Mc_4271-R1.fastq.1
-curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984973/Mc_4271-R2.fastq.1
-
-curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984974/Mc_4297-R1.fastq.1
-curl -o https://sra-pub-src-2.s3.amazonaws.com/SRR9984974/Mc_4297-R2.fastq.1
 
 
-# Convert SRA to SAM and directly into BAM for FastQC and other tools
-#for f in SRA_Files/Raw_BAM_Files/*; do sam-dump ${f} | samtools view -bS - > "${f}.bam"; done
+# Setup directory tree and move to working directory. Accession number d
+mkdir -p RNA_Seq_Run_${currentDate}/${SRA_Accession_Number}/{SRA_Files/{Raw_Files,Trimmed_Files,QC_Output},Aligner_Output/{Trinity,BowTie,STAR,Oasis,QC}}
+cd RNA_Seq_Run_${currentDate}/${SRA_Accession_Number}/
 
 
-# Pass split PE reads to trimmomatic
-#   use anon named pipes to pass data in <() or out >()
->>>>>>> 1a0ac70d725d288c7a55f96412a8ac86dcb548c3
+# Dump split fastq reads for trimmomatic
+fastq-dump --split-files -O SRA_Files/Raw_Files/ ${SRA_Accession_Number}
+
+# Re-download to perform hash check
+mkdir -p ./temp/Raw_Files/
+fastq-dump --split-files -O ./temp/Raw_Files/ ${SRA_Accession_Number}
+
+# If hashsum values are not equal re-download up to 10 times, else quit with error message
+loopCount=0
+while [[ 0x`openssl dgst -sha1 -binary ./SRA_Files/Raw_Files/* | xxd -p -c 1000` -ne 0x`openssl dgst -sha1 -binary ./SRA_Files/Raw_Files/* | xxd -p -c 1000` ]]
+do
+rm ./SRA_Files/Raw_Files/*
+mv ./temp/Raw_Files/* ./SRA_Files/Raw_Files/
+fastq-dump --split-files -O ./temp/Raw_Files/ ${SRA_Accession_Number}
+loopCount=$((loopCount+1))
+done
+
+rm -r ./temp/Raw_Files/
+
+echo "done!"
