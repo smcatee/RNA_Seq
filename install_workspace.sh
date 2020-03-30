3929012ae3c94ebcb4c608a76ff35b84fde83857
Sean David McAtee <sean.d.mcatee@gmail.com>
2020-03-29 20:44:12 -0700

    up to trinity tested, working

--- install_workspace.sh
+++ install_workspace.sh
@@ -2,7 +2,7 @@
 
 
 # After updating and downloading git
-sudo apt-get -y install make gcc zlib1g-dev libncurses5-dev libncursesw5-dev cmake build-essential unzip python-dev python-numpy python3-dev python3-pip gfortran libreadline-dev default-jdk libx11-dev libxt-dev xorg-dev libxml2-dev libcurl4-openssl-dev apache2 python-pip csh ruby-full gnuplot cpanminus r-base libssl-dev gcc-4.8 g++-4.8
+sudo apt-get -y install make gcc zlib1g-dev libncurses5-dev libncursesw5-dev cmake build-essential unzip python-dev python-numpy python3-dev python3-pip gfortran libreadline-dev default-jdk libx11-dev libxt-dev xorg-dev libxml2-dev libcurl4-openssl-dev apache2 python-pip csh ruby-full gnuplot cpanminus r-base libssl-dev gcc-4.8 g++-4.8 libtbb-dev
 
 sudo timedatectl set-timezone America/Los_Angeles
 
@@ -29,9 +29,28 @@ cd /opt
 # SRA Toolkit
 wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.10.4/sratoolkit.2.10.4-ubuntu64.tar.gz
 tar -zxvf sratoolkit*.tar.gz
-PATH="${PATH}:/opt/sratoolkit.2.10.4-ubuntu64/bin"
-source /etc/environment && export PATH
+# set PATH
+echo ' '  >> /home/ubuntu/.profile
+echo '# set PATH for SRAtoolkit' >> /home/ubuntu/.profile
+echo 'export PATH="${PATH}:/opt/sratoolkit.2.10.4-ubuntu64/bin"' >> /home/ubuntu/.profile
+source ~/.profile
+source ~/.bashrc
 vdb-config --restore-defaults
+
+# Samtools
+wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2
+tar -xvjf samtools*.tar.bz2
+./samtools-1.10/configure --prefix=/opt/samtools*/
+cd samtools*/
+make
+make install
+# set PATH
+echo ' '  >> /home/ubuntu/.profile
+echo '# set PATH for Samtools' >> /home/ubuntu/.profile
+echo 'export PATH="${PATH}:/opt/samtools-1.10/bin"' >> /home/ubuntu/.profile
+source ~/.profile
+source ~/.bashrc
+
 
 # FastQC
 wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.9.zip
@@ -59,6 +78,35 @@ add-apt-repository ppa:kelleyk/emacs
 add-apt-repository ppa:jonathonf/gcc-7.1  #maybe not necessary
 apt update
 apt-get -y install gmap bowtie2 emacs25 gcc-7 g++-7 # Dependencies
+wget https://github.com/gmarcais/Jellyfish/releases/download/v2.2.10/jellyfish-2.2.10.tar.gz
+tar -zxvf jellyfish*.gz
+cd jellyfish*/
+./configure
+make
+make install
+sudo ldconfig
+cd ..
+# set PATH
+echo ' '  >> /home/ubuntu/.profile
+echo '# set PATH for Jellyfish' >> /home/ubuntu/.profile
+echo 'export PATH="${PATH}:/opt/jellyfish-2.2.10/bin"' >> /home/ubuntu/.profile
+source ~/.profile
+source ~/.bashrc
+wget https://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.4.1/bowtie2-2.4.1-source.zip
+unzip bowtie2-2.4.1-source.zip
+cd bowtie2*/
+make
+cd ..
+
+wget https://github.com/COMBINE-lab/salmon/releases/download/v1.1.0/salmon-1.1.0_linux_x86_64.tar.gz
+tar -zxvf salmon*.tar.gz
+# set PATH
+echo ' '  >> /home/ubuntu/.profile
+echo '# set PATH for Salmon' >> /home/ubuntu/.profile
+echo 'export PATH="${PATH}:/opt/salmon-latest_linux_x86_64/bin"' >> /home/ubuntu/.profile
+source ~/.profile
+source ~/.bashrc
+
 wget https://github.com/trinityrnaseq/trinityrnaseq/releases/download/v2.10.0/trinityrnaseq-v2.10.0.FULL.tar.gz
 tar -zxvf ./trinity*.tar.gz
 cd trinityrnaseq*/
@@ -66,6 +114,7 @@ chmod a+w ./trinity-plugins/bamsifter/Makefile
 sed -i 's/g++/g++ -std=c++11/' ./trinity-plugins/bamsifter/Makefile
 make
 cd ..
+
 
 # Oasis (requires Velvet, TexLive)
 apt-get -y install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
