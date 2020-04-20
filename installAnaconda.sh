9c10f68b3a1f04c6be32bf3aec673bc0fffdb638
Sean David McAtee <sean.d.mcatee@gmail.com>
2020-04-20 14:46:21 -0700

    installAnaconda

--- installAnaconda.sh
+++ installAnaconda.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+cur -O https://repo.anaconda.com/archive/Anaconda2-2019.10-Linux-x86_64.sh
